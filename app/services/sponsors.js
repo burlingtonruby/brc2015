999f102e524e6bdb68cc62293a988e33298745be
Brett Chalupa <brett@brettchalupa.com>
2015-05-22 12:30:42 -0400

    Add WebLinc sponsor
    Adjust the Travis CI logo to be uniform in size together.

--- app/services/sponsors.js
+++ app/services/sponsors.js
@@ -8,6 +8,12 @@ const SPONSORS = [
     logoPath: new SafeString('/images/sponsors/travis-ci.png'),
     name: 'Travis CI',
     website: 'https://travis-ci.com'
+  },
+  {
+    level: 'small',
+    logoPath: new SafeString('/images/sponsors/weblinc.png'),
+    name: 'WebLinc',
+    website: 'http://www.weblinc.com'
   }
 ];
 
